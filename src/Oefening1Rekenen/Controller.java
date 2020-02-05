08dee311c95e640aa726e897ebf708b71c446225
bram <bram.declercq2@student.vives.be>
2020-02-05 13:48:41 +0100

    Plus ingevoegd

--- src/Oefening1Rekenen/Controller.java
+++ src/Oefening1Rekenen/Controller.java
@@ -2,9 +2,25 @@ package Oefening1Rekenen;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     public Label lblUitkomst;
     public TextField txtGetal1;
     public TextField txtGetal2;
+
+    public void btnPlus(MouseEvent mouseEvent) {
+        plus();
+
+    }
+
+
+    double plus() {
+        double Getaluitkomst;
+        Getaluitkomst = Double.valueOf(txtGetal1.getText()) + Double.valueOf(txtGetal2.getText());
+        lblUitkomst.setText(String.valueOf(Getaluitkomst));
+        return Getaluitkomst;
+    }
+
 }
+
