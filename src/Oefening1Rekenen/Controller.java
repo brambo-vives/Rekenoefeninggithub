5be88f4093922aa2f93f16cb6d66594546151040
bram <bram.declercq2@student.vives.be>
2020-02-05 14:04:50 +0100

    Foutafhandeling

--- src/Oefening1Rekenen/Controller.java
+++ src/Oefening1Rekenen/Controller.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import javax.swing.*;
+
 public class Controller {
     public Label lblUitkomst;
     public TextField txtGetal1;
@@ -15,12 +17,17 @@ public class Controller {
     }
 
 
-    double plus() {
-        double Getaluitkomst;
-        Getaluitkomst = Double.valueOf(txtGetal1.getText()) + Double.valueOf(txtGetal2.getText());
-        lblUitkomst.setText(String.valueOf(Getaluitkomst));
-        return Getaluitkomst;
+   public void  plus() {
+        try {
+            double Getaluitkomst;
+            Getaluitkomst = Double.valueOf(txtGetal1.getText()) + Double.valueOf(txtGetal2.getText());
+            lblUitkomst.setText(String.valueOf(Getaluitkomst));
+
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null, "gelieve een getal op te geven");
+        }
     }
+
 
 }
 
