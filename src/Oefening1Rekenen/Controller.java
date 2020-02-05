2291d6b8c49f1fde9fe8533d69ad0cfe0fbbf847
bram <bram.declercq2@student.vives.be>
2020-02-05 11:48:39 +0100

    Initial commit

---
+++ src/Oefening1Rekenen/Controller.java
@@ -0,0 +1,10 @@
+package Oefening1Rekenen;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public Label lblUitkomst;
+    public TextField txtGetal1;
+    public TextField txtGetal2;
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module Rekenoefeninggithub {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+    opens Oefening1Rekenen;
+}
