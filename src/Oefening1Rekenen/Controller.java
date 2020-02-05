c533eed41703db8a212a1c7751d2ea6db6a9db4a
Reynaert Geoffrey <Geoffrey.reynaert@student.vives.be>
2020-02-05 20:51:40 +0100

    CodeCleaning

--- src/Oefening1Rekenen/Controller.java
+++ src/Oefening1Rekenen/Controller.java
@@ -10,22 +10,20 @@ public class Controller {
     public Label lblUitkomst;
     public TextField txtGetal1;
     public TextField txtGetal2;
+
     public void btnPlus(MouseEvent mouseEvent) {
         plus();
-
     }
-
    public void  plus() {
-
             double Getaluitkomst;
         try {
             Getaluitkomst = Double.valueOf(txtGetal1.getText()) + Double.valueOf(txtGetal2.getText());
-
             lblUitkomst.setText(String.valueOf(Getaluitkomst));
         } catch (NumberFormatException ex) {
             JOptionPane.showMessageDialog(null, "gelieve een getal op te geven");
         }
     }
+    
     public void myMinFunction(double num1,double num2){
         double dResult;
         dResult= num1-num2;
