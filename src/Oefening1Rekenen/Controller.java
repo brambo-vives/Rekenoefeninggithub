bdf5102c3d4af2387301344a7e9d12c718282e48
Femke Vermander <femke.vermander@student.vives.be>
2020-02-05 14:07:37 +0100

    FunctieMaal

--- src/Oefening1Rekenen/Controller.java
+++ src/Oefening1Rekenen/Controller.java
@@ -2,9 +2,24 @@ package Oefening1Rekenen;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     public Label lblUitkomst;
     public TextField txtGetal1;
     public TextField txtGetal2;
+
+    public void clicked_Maal(MouseEvent mouseEvent) {
+        FunctieMaal();
+    }
+
+    //Fucntie voor maal
+    private void FunctieMaal() {
+        double dGetal1 = Double.parseDouble(txtGetal1.getText());
+        double dGetal2 = Double.parseDouble(txtGetal2.getText());
+
+        double dMaal = dGetal1 * dGetal2;
+
+        lblUitkomst.setText(String.valueOf(dMaal));
+    }
 }
