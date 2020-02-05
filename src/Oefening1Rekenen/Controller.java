e53b3bcd51c4cc9aa712f287f9c02cd7ec64cde8
NielsLefevre <r0710934@student.vives.be>
2020-02-05 13:49:52 +0100

    Rest berekenen
    Methode om rest te bereken tussen 2 ingegeven getallen.

--- src/Oefening1Rekenen/Controller.java
+++ src/Oefening1Rekenen/Controller.java
@@ -2,9 +2,21 @@ package Oefening1Rekenen;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
     public Label lblUitkomst;
     public TextField txtGetal1;
     public TextField txtGetal2;
+
+    public void Clicked_Rest(MouseEvent mouseEvent) {
+        geefRest();
+    }
+
+    public void geefRest(){
+        double getal1 = Double.parseDouble(txtGetal1.getText());
+        double getal2 = Double.parseDouble(txtGetal2.getText());;
+        double rest = getal1 % getal2;
+        lblUitkomst.setText(String.valueOf(rest));
+    }
 }
